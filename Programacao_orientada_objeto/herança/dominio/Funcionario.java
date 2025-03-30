d7c066eb82c052cfc5bc24d0a86db45b19fa98c9
Alisson Ricarte <alissonricarte7575@gmail.com>
2025-03-30 15:51:41 -0300

    test: Finalizando Estudo de Herança

--- Programacao_orientada_objeto/herança/dominio/Funcionario.java
+++ Programacao_orientada_objeto/herança/dominio/Funcionario.java
@@ -3,9 +3,15 @@ package Programacao_orientada_objeto.herança.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
+    public Funcionario(String nome){
+        super(nome);
+    }
     public void imprime(){
         super.imprime();
         System.out.println(this.salario);
+    }
+    public void imprimeRelatorio(){
+        System.out.println("EU: "+this.nome+ " RECEBIR: "+this.salario);
     }
     public double getSalario() {
         return salario;
--- Programacao_orientada_objeto/herança/main/test01.java
+++ Programacao_orientada_objeto/herança/main/test01.java
@@ -4,20 +4,37 @@ import Programacao_orientada_objeto.herança.dominio.Endereco;
 import Programacao_orientada_objeto.herança.dominio.Funcionario;
 import Programacao_orientada_objeto.herança.dominio.Pessoa;
 
+/*Inicialização de atributos estáticos da superclasse.
+
+Blocos estáticos da superclasse são executados.
+
+Inicialização de atributos estáticos da subclasse.
+
+Blocos estáticos da subclasse são executados.
+
+Inicialização de atributos de instância da superclasse.
+
+Blocos de inicialização da superclasse são executados.
+
+Construtor da superclasse é chamado.
+
+Inicialização de atributos de instância da subclasse.
+
+Blocos de inicialização da subclasse são executados.
+
+Construtor da subclasse é chamado. */
 public class test01 {
     public static void main(String[] args) {
         Endereco endereco = new Endereco();
         endereco.setRua("silva");
         endereco.setCep("287625-484");
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome("pedro");
+        Pessoa pessoa = new Pessoa("pedro");
         pessoa.setCpf("637.828.273-82");
         pessoa.setEndereco(endereco);
 
         pessoa.imprime();
 
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("maria");
+        Funcionario funcionario = new Funcionario("maria");
         funcionario.setCpf("139.828.284-23");
         funcionario.setEndereco(endereco);
         funcionario.setSalario(1450);
